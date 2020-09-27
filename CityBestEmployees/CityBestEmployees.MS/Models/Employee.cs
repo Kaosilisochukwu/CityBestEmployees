987db336c77063d956ff77a2932981afa1842d43
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-09-28 00:42:01 +0100

    Add Model classes

---
+++ CityBestEmployees/CityBestEmployees.MS/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityBestEmployees.MS.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+        public string Photo { get; set; }
+        public int GenderId { get; set; }
+        public Gender Gender { get; set; }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+
+    }
+}
---
+++ CityBestEmployees/CityBestEmployees.MS/Models/Gender.cs
@@ -0,0 +1,9 @@
+﻿namespace CityBestEmployees.MS.Models
+{
+    public class Gender
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
